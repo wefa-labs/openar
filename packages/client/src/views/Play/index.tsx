68e4eb72ccbd1b23f25266bcb850abb999e93327
Afo <obaone01@gmail.com>
2023-05-23 08:44:44 -0700

    updates

--- packages/client/src/views/Play/index.tsx
+++ packages/client/src/views/Play/index.tsx
@@ -1,9 +1,24 @@
-import Incrementer from "./Increment";
+import { ARButton, XR } from "@react-three/xr";
+import { Canvas } from "@react-three/fiber";
 
 export default function Play() {
   return (
     <>
-      <Incrementer />
+      <ARButton />
+      <Canvas
+        // onCreated={onCanvasCreated}
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <XR>
+          <Board position={[0, 0, -1]}></Board>
+        </XR>
+      </Canvas>
     </>
   );
 }
