3daab9a07f9f5a83b47e375554b02da3f20b9a98
Afo <obaone01@gmail.com>
2023-06-03 01:55:46 -0700

    updates

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,33 +1,22 @@
 // import fs from "fs";
 import dotenv from "dotenv";
 // import Redis from "ioredis";
-// import RedisStore from "connect-redis";
-import session from "@fastify/session";
+import RedisStore from "connect-redis";
+import session, { SessionStore } from "@fastify/session";
 
 dotenv.config();
 
 import { router } from "./router";
 import { server } from "./server";
+import { redis } from "./modules/redis";
 // import path from "path";
 
 const IS_GOOGLE_CLOUD_RUN = process.env.K_SERVICE !== undefined;
 const host = IS_GOOGLE_CLOUD_RUN ? "0.0.0.0" : undefined;
 const port = Number(process.env.PORT) || 3008;
 
-// const redis = new Redis({
-//   port: 6379,
-//   username: "",
-//   host: process.env.REDIS_HOST,
-//   password: process.env.REDIS_PASSWORD,
-//   tls: {
-//     ca: [fs.readFileSync(path.join(__dirname, "../cert/sg-wefa-cache.cert"), "ascii")],
-//   },
-//   connectTimeout: 20000,
-//   // family: 6,
-// });
-
 // Middleware
-// server.register(require("@fastify/redis"), { client: redis, host: process.env.REDIS_HOST });
+// server.register(require("@fastify/redis"), { client: redis, closeClient: true });
 server.register(require("@fastify/cors"), { origin: "*" }); // Adjust the "origin" option as needed
 server.register(require("@fastify/helmet"));
 server.register(require("@fastify/sensible"));
@@ -40,10 +29,10 @@ server.register(session, {
     maxAge: 86400000, // 1 day
     httpOnly: true,
   },
-  // store: new RedisStore({
-  //   client: redis,
-  //   prefix: "wefa:",
-  // }) as SessionStore,
+  store: new RedisStore({
+    client: redis,
+    prefix: "wefa:",
+  }) as SessionStore,
 });
 server.register(require("@fastify/multipart"));
 
