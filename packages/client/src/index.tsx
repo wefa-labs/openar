da192669a97501d9e95faf3aa8186ad12546cb3d
Afo <obaone01@gmail.com>
2023-06-04 04:45:51 -0700

    added sentry

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
+import * as Sentry from "@sentry/react";
 // import { mount as mountDevTools } from "@latticexyz/dev-tools";
 
 import App from "./App";
@@ -8,6 +9,28 @@ import { ComingSoon } from "./components/Layout/ComingSoon";
 
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
+import "react-spring-bottom-sheet/dist/style.css";
+
+Sentry.init({
+  dsn: "https://examplePublicKey@o0.ingest.sentry.io/0",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 const rootElement = document.getElementById("react-root");
 if (!rootElement) throw new Error("React root not found");
