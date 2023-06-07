829bc1c740652bb49d071d3b5b07e3a17cb3b23d
Afo <obaone01@gmail.com>
2023-06-07 14:17:58 -0700

    fixed bug

--- packages/creature-generator/generator.py
+++ packages/creature-generator/generator.py
@@ -1,10 +1,10 @@
 import os
 import requests
-import base64
 import json
 
 from io import BytesIO
 from PIL import Image
+from base64 import b64encode
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")  # config = {"USER": "foo", "EMAIL": "foo@example.org"}
@@ -63,6 +63,7 @@ creatures_dic = {
 }
 
 GENERATOR_GPU_URL = os.environ.get("GENERATOR_GPU_URL", "http://75.191.38.75:40789")
+ENCODING = 'utf-8'
 
 #stable diffusion
 # this is subject to change so maybe use kwargs
@@ -75,7 +76,10 @@ async def generate_creature_route(creature_type, element_type, description, cach
 
         url = GENERATOR_GPU_URL + '/sdapi/v1/txt2img' 
         with open(this_creature['template'] , "rb") as image_file:
-            control_img = base64.b64encode(image_file.read())
+            byte_content = image_file.read()
+            
+        base64_bytes = b64encode(byte_content)
+        base64_string = base64_bytes.decode(ENCODING)
 
         # TODO: Update the prompt grae
         dics = {
@@ -99,7 +103,7 @@ async def generate_creature_route(creature_type, element_type, description, cach
             "alwayson_scripts": {
             	"controlnet":{
             	    "args":[{
-            	    "input_image": control_img,
+            	    "input_image": base64_string,
             	    "module": 'seg_ofcoco',
             	    "model": 'control_sd15_seg [fef5e48e]',
             	    "weight": 2,
