8cfd3736d67b1153fe1ec5f966baac7936133b13
Afo <obaone01@gmail.com>
2023-06-16 00:24:17 -0700

    update

--- packages/client/src/components/Deck/Viewer.tsx
+++ packages/client/src/components/Deck/Viewer.tsx
@@ -68,7 +68,10 @@ export const DeckViewer: React.FC<DeckViewerProps> = ({
     <BottomSheet
       // className="fixed bottom-0 left-0 right-0 z-10 mb-16 flex h-screen touch-pan-y flex-col gap-4 rounded-lg p-4 "
       // style={style}
-      className="z-20 fixed min-h-5/6 max-h-5/6 bg-base-100"
+      style={{
+        maxHeight: "90vh",
+      }}
+      className="z-20 fixed bg-base-100"
       open={open}
       onDismiss={onDismiss}
 
